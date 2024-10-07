0f8fc9bcdcd901ad0a29453c2001713fc2f0168c
[ JOKER ] <kanokpol.t43@gmail.com>
2024-10-07 17:22:13 +0700

    Validate result

--- cypress/integration/examples/Test6.js
+++ cypress/integration/examples/Test6.js
@@ -4,9 +4,12 @@ describe("Framework", () => {
       this.data = data;
     });
   });
-  it("Hook Fucntion", function(){
+  it("Hook Fucntion", function () {
     cy.visit("https://rahulshettyacademy.com/angularpractice/");
     cy.get('[name="name"]').eq(0).type(this.data.name);
     cy.get("#exampleFormControlSelect1").select(this.data.gender);
+    cy.get('[name="name"]').eq(1).should("have.value", this.data.name);
+    cy.get('[name="name"]').eq(0).should("have.attr", "minlength", 2);
+    cy.get("#inlineRadio3").should("be.disabled");
   });
 });
