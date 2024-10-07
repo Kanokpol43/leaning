7074b56e171e7c2e4ef9e801a6c78c4edf3bb618
[ JOKER ] <kanokpol.t43@gmail.com>
2024-10-07 22:56:25 +0700

    Click button dynamic

--- cypress/integration/examples/Test6.js
+++ cypress/integration/examples/Test6.js
@@ -12,4 +12,14 @@ describe("Framework", () => {
     cy.get('[name="name"]').eq(0).should("have.attr", "minlength", 2);
     cy.get("#inlineRadio3").should("be.disabled");
   });
+
+  it("", function () {
+    cy.visit("https://rahulshettyacademy.com/angularpractice/");
+    cy.get(":nth-child(2) > .nav-link").click();
+    cy.get("h4.card-title").each((title, index) => {
+      if (title.text().includes("Blackberry")) {
+        cy.get(".btn-info").eq(index).click()
+      }
+    });
+  });
 });
