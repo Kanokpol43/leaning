5de01fa1bc3036108477b69351323cebfbe4d7dc
[ JOKER ] <kanokpol.t43@gmail.com>
2024-10-08 16:10:22 +0700

    Import-and-use-class-fucntion

--- cypress/integration/examples/Test6.js
+++ cypress/integration/examples/Test6.js
@@ -1,3 +1,6 @@
+import HomePage from "../pageObjects/HomePage";
+import ProductPage from "../pageObjects/ProductPage";
+
 describe("Framework", () => {
   before(function () {
     cy.fixture("example").then(function (data) {
@@ -13,7 +16,7 @@ describe("Framework", () => {
     cy.get("#inlineRadio3").should("be.disabled");
   });
 
-  it.only("Click button dynamic", function () {
+  it("Click button dynamic", function () {
     cy.visit("https://rahulshettyacademy.com/angularpractice/");
     cy.get(":nth-child(2) > .nav-link").click();
 
@@ -28,5 +31,41 @@ describe("Framework", () => {
     cy.pause();
     cy.selectProduct("Blackberry");
     cy.selectProduct("Nokia");
+  });
+});
+
+describe.only("Import and use Class", () => {
+  let homepage, productpage;
+
+  beforeEach(function () {
+    cy.fixture("example").then(function (data) {
+      this.data = data;
+    });
+
+    cy.visit("https://rahulshettyacademy.com/angularpractice/");
+  });
+
+  before(function () {
+    homepage = new HomePage();
+    productpage = new ProductPage();
+  });
+
+  it("Home Page", function () {
+    homepage.getEditBox().type(this.data.name);
+    homepage.getGender().select(this.data.gender);
+    homepage.getTwoWayDataBiding().should("have.value", this.data.name);
+    homepage.getEditBox().should("have.attr", "minlength", 2);
+    homepage.getEntrepreneur().should("be.disabled");
+
+    homepage.getShopTab().click();
+  });
+
+  it("Product Page", function () {
+    productpage.checkOutButton().click();
+    this.data.productName.forEach(function (element) {
+      // เรียกฟังก์ชัน cy.selectProduct สำหรับแต่ละ element (สินค้า)
+      // โดยในแต่ละรอบ element จะเป็นชื่อสินค้า เช่น "Blackberry" ในรอบแรก และ "Nokia" ในรอบถัดไป
+      cy.selectProduct(element);
+    });
   });
 });
