2f6962e3df23f48b0e85b0b6dfe65dfa175995be
[ JOKER ] <kanokpol.t43@gmail.com>
2024-10-08 11:48:05 +0700

    how to use forEach command

--- cypress/integration/examples/Test6.js
+++ cypress/integration/examples/Test6.js
@@ -13,10 +13,18 @@ describe("Framework", () => {
     cy.get("#inlineRadio3").should("be.disabled");
   });
 
-  it("Click button dynamic", function () {
+  it.only("Click button dynamic", function () {
     cy.visit("https://rahulshettyacademy.com/angularpractice/");
     cy.get(":nth-child(2) > .nav-link").click();
 
-    cy.selectProduct("Note");
+    // วนลูปผ่าน array ของชื่อสินค้าใน this.data.productName (คือ ["Blackberry", "Nokia"])
+    this.data.productName.forEach(function (element) {
+      // เรียกฟังก์ชัน cy.selectProduct สำหรับแต่ละ element (สินค้า)
+      // โดยในแต่ละรอบ element จะเป็นชื่อสินค้า เช่น "Blackberry" ในรอบแรก และ "Nokia" ในรอบถัดไป
+      cy.selectProduct(element);
+    });
+
+    cy.selectProduct("Blackberry");
+    cy.selectProduct("Nokia");
   });
 });
